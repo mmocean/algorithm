a5aa45f03af38b47ac4fc7e7568be08d7b0a8c9d
lihaiyangcsu@gmail.com <lihaiyangcsu@gmail.com>
2015-07-01 09:26:19 +0800

    new-added bf alongside kmp

--- testkmp.cpp
+++ testkmp.cpp
@@ -6,10 +6,11 @@
  ************************************************************************/
 
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 
 
-int getnext( const char *str, int size )
+inline int getnext( const char *str, int size )
 {
 	for( int i = 1; i<size; i++ )
 	{
@@ -29,21 +30,21 @@ int kmp( const char* src, const char* pattern )
 		return -1;
 	
 	const char* head = src;
-	int len = strlen(pattern);
-	
 	int count = 0;
+		
 	while( '\0' != *src )
 	{
 		int next = (count > 0)?getnext( pattern, count ):0;
-		count = count - next;
+		count = next;
+		const char* subfix = src-count;
 		while( '\0' != *src && '\0' != *(pattern+next) && (*src == *(pattern+next)) )
 		{
 			++next;
 			++count;
 			++src;
 		}
-		if( len == next )
-			return (src-head-len);
+		if( '\0' == *(pattern+next) )
+			return (subfix-head);
 		if( 0 == next )
 			++src;
 	}
@@ -51,18 +52,46 @@ int kmp( const char* src, const char* pattern )
 }
 
 
-int main()
+int bf( const char* src, const char* pattern )
 {
-	const char* str = "ababdababc";
-	const char* p = "bd";
+	if( NULL == src || NULL == pattern )
+		return -1;
 
-	printf( "str: %s\n", str );
-	printf( "p:   %s\n", p );
+	const char* head = src;
+	
+	while( '\0' != *src )
+	{
+		const char* tmps = src;
+		const char* tmpp = pattern;
+		while( '\0' != *src && '\0' != *tmpp && (*src == *tmpp) )
+		{
+			++src;
+			++tmpp;
+		}
+		if( '\0' == *tmpp )	
+			return (tmps-head);
+		src = ++tmps;	
+	}
+
+	return -1;
+}
+
+
+int main( int argc, char **argv )
+{
+	if( argc < 3 )
+		exit(-1);
+
+	const char* p = argv[1];
+	const char* str = argv[2];
+
+	printf( "strlen str: %d\n", strlen(str) );
 
 	int ret = getnext( p, strlen(p) );
 	printf( "ret %d\n", ret );
 
-	ret = kmp( str, p );
+	//ret = kmp( str, p );
+	ret = bf( str, p );
 	printf( "subfix %d\n", ret );
 
 	return 0;
