864f4e91095ba17f787ca9017d3722f483ea921b
lihaiyangcsu@gmail.com <lihaiyangcsu@gmail.com>
2015-07-18 20:59:48 +0800

    quicksort:improvement

--- testquicksort.cpp
+++ testquicksort.cpp
@@ -12,32 +12,27 @@ int partiton( int array[], int low, int high )
 	int key = *(array+low);
 	while( low < high )
 	{
-		while( low < high && key < *(array+high) )
+		while( low < high && key <= *(array+high) )
 			--high;	
-		int tmp = *(array+high);
-		*(array+high) = key;
-		*(array+low) = tmp;
+		*(array+low) = *(array+high);
 	
-		while( (low+1) < high && key > *(array+low+1) )
+		while( low < high && key >= *(array+low) )
 			++low;
-		tmp = *(array+high);
-		*(array+high) = *(array+low+1);
-		*(array+low+1) = tmp;
-		
-		++low;
-		--high;
+		*(array+high) = *(array+low);	
 	}
+	*(array+low) = key;
+
 	return low;
 }
 
 
-int merge( int array[], int low, int high )
+int quicksort( int array[], int low, int high )
 {
 	if( low < high )
 	{
 		int index = partiton( array, low, high );
-		merge( array, low, index-1 );
-		merge( array, index+1, high );
+		quicksort( array, low, index-1 );
+		quicksort( array, index+1, high );
 	}
 	return 0;
 }
@@ -46,7 +41,7 @@ int merge( int array[], int low, int high )
 int main()
 {
 	int array[] = { 12,3,34,56,12,8,9,100 };
-	int ret = merge( array, 0, sizeof(array)/sizeof(int)-1 );
+	int ret = quicksort( array, 0, sizeof(array)/sizeof(int)-1 );
 	for( int i = 0; i<sizeof(array)/sizeof(int); i++ )
 	{
 		printf( "%d ", *(array+i) );
